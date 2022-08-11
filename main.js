7fbb20cc31ac51b4501db0bce6951c8c3d92c8c8
maiguelm <migmoyano86@gmail.com>
2022-08-11 19:53:11 -0300

    commit-modal

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ const carrito = [];
 const usuario = [];
 
 class Pasteleria {
-	constructor(id, imagen, nombre, precio, descripcion){
+	constructor(id, imagen, nombre, precio, descripcion, cantidad){
         this.id = id;
         this.imagen = imagen;
 		this.nombre = nombre.toUpperCase();
 		this.precio = parseFloat(precio);
         this.descripcion = descripcion;
+        this.cantidad = cantidad
 	}
 	mostrarPasteleria(){
 		alert(this.nombre + " $ " +this.precio);
@@ -25,20 +26,20 @@ class Pasteleria {
 }
 
 class CajaTortas extends Pasteleria {
-    constructor(id, imagen, nombre, precio, descripcion, sabor){
-        super(id, imagen, nombre, precio, descripcion, sabor)
+    constructor(id, imagen, nombre, precio, descripcion,cantidad, sabor){
+        super(id, imagen, nombre, precio, descripcion, cantidad)
         this.sabor = sabor;
     }
 }
 
 
 
-const croissant = new Pasteleria (1,'./images/croissant.jpg', "Croissant", 250, "Un verdadero Croissant de manteca, con un proceso de elaboracion de tres días");
-const panChocolat = new Pasteleria (2, "./images/pain.jpg","Pan Chocolate", 280, "Un delicioso Pain Aux Chocolat, una especialidad verdaderamente hojaldrada");
-const chausson = new Pasteleria (3, "./images/chausson.jpg" ,"Chausson", 350, "Exisita Vionnoserie rellena de una compoa de manzanas y canela");
-const balcarce = new Pasteleria (4, "./images/balcarce.jpg","Postre Balcarce", 1250, "Clásico postre elaborado con merengue, crema de leche,dulce de leche, piononos y nueces. Extra-sweet!");
-const alfajor = new Pasteleria (5, "./images/alfajor.jpg","Alfajor", 300, "Otro clásico: el alfajor marplatense3, relleno de dulce de leche y bañado en chocolate cobretura semi-amargo");
-const cajaTortitas = new CajaTortas (6, "./images/cajatortitas.jpg","Caja de Tortitas", 1000, "Exclusiva Caja de Tortitas, de seis unidades a eleccion! Se puede elegir entre varios sabores! ", saboresTortitas);
+const croissant = new Pasteleria (1,'./images/croissant.jpg', "Croissant", 250, "Un verdadero Croissant de manteca, con un proceso de elaboracion de tres días", 1);
+const panChocolat = new Pasteleria (2, "./images/pain.jpg","Pan Chocolate", 280, "Un delicioso Pain Aux Chocolat, una especialidad verdaderamente hojaldrada", 1);
+const chausson = new Pasteleria (3, "./images/chausson.jpg" ,"Chausson", 350, "Exisita Vionnoserie rellena de una compoa de manzanas y canela", 1);
+const balcarce = new Pasteleria (4, "./images/balcarce.jpg","Postre Balcarce", 1250, "Clásico postre elaborado con merengue, crema de leche,dulce de leche, piononos y nueces. Extra-sweet!", 1);
+const alfajor = new Pasteleria (5, "./images/alfajor.jpg","Alfajor", 300, "Otro clásico: el alfajor marplatense3, relleno de dulce de leche y bañado en chocolate cobretura semi-amargo", 1);
+const cajaTortitas = new CajaTortas (6, "./images/cajatortitas.jpg","Caja de Tortitas", 1000, "Exclusiva Caja de Tortitas, de seis unidades a eleccion! Se puede elegir entre varios sabores! ", 1, saboresTortitas);
 
 
 
@@ -142,8 +143,13 @@ console.log(nuevosPrecios);
 
 const cartas = document.getElementById("cartas");
 const cargaCartas = document.createDocumentFragment();
+const contenedorCarrito = document.getElementById("carritoContenedor");
+const vaciarCarrito = document.getElementById("vaciar-carrito");
+const itemsCarrito = document.getElementById("cart-items");
+const precioFinal = document.getElementById("precioTotal");
+const cantidad = document.getElementById("cantidad");
 
-for (const producto of productos){
+productos.forEach((producto) => {
     let carta = document.createElement("div")
     carta.classname = "card h-100";
     carta.innerHTML = `
@@ -162,29 +168,106 @@ for (const producto of productos){
 
     const botonAgregar = document.getElementById(`agregar${producto.id}`);
 
-    botonAgregar.addEventListener("click", respuesta);
-        function respuesta (){
-            console.log("Agregaste el producto al carrito");
-            carrito.push(producto.nombre);
-        }
+    botonAgregar.addEventListener("click", () => {
+        agregarAlCarrito(producto.id)
+    })
+
+
+    // botonAgregar.addEventListener("click", respuesta);
+    //     function respuesta (){
+    //         console.log("Agregaste el producto al carrito");
+    //         carrito.push(producto.nombre);
+    //     }
+})
+
+const agregarAlCarrito = (prodId) => {
+    const existe = carrito.some (prod => prod.id === prodId)
+    if (existe){ 
+        const prod = carrito.map (prod => { 
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    } else { 
+        const item = productos.find ((prod) => prod.id === prodId);
+        carrito.push(item);
+    }
+
+    enElCarrito();
+    console.log(carrito);
 }
+
+const enElCarrito = () => {
+    contenedorCarrito.innerHTML = "";
+    carrito.forEach((prod) => {
+        const div = document.createElement("div")
+        div.className = ("productoEnCarrito")
+        div.innerHTML =  `
+        <p class="pModal">${prod.nombre} </p>
+        <p class="pModal">Precio: $ ${prod.precio}</p>
+        <p class="pModal">Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="sacarCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i>Eliminar</button>
+        `
+        contenedorCarrito.appendChild(div)
+    })
+    itemsCarrito.innerText = carrito.length
+    precioFinal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0);
+}
+
+const sacarCarrito = (prodId) =>{
+    const item = carrito.find ((prod) => prod.id === prodId);
+    const i = carrito.indexOf(item);
+    carrito.splice(i, 1)
+    enElCarrito();
+    console.log(carrito)
+}
+
+vaciarCarrito.addEventListener("click", () =>{
+    carrito.length = 0;
+    enElCarrito();
+})
+
+
 
 const botonBanner = document.querySelector(".banner-btn")
 
-botonBanner.onclick = () => {
+botonBanner.addEventListener ("mousedown", () => {
     location.href = "#productos";
-}
+})
 
 
 const formularioNombre = document.querySelector(".nombreForm");
 const telefonoFormulario = document.querySelector(".telForm");
+const mensajes = document.getElementById("mensajes")
 
 formularioNombre.onchange = function () {
     console.log(formularioNombre.value);
     usuario.push(formularioNombre.value)
 }
 
+formularioNombre.addEventListener ("select",(e) =>{
+    let comienzo = e.target.selectionStart;
+    let fin = e.target.sectionEnd;
+    let nombreFinal = formularioNombre.value;
+    mensajes.innerHTML = nombreFinal.substring(comienzo, fin);
+})
+
 telefonoFormulario.onchange = function () {
     console.log(telefonoFormulario.value);
     usuario.push(telefonoFormulario.value)
 }
+
+const openModal = document.querySelector('.abrirCarrito');
+const modal = document.querySelector('.miModal');
+const closeModal = document.querySelector('.cerrarCarrito');
+
+openModal.addEventListener('click', (e)=>{
+    alert("hiciste click")
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
